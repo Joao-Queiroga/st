20d4181fba37ff9aceee8b3828a8d4d0780076cb
joaoqueiroga <queiroga.joaofcsol@gmail.com>
2023-01-24 16:40:50 -0300

    added cflags for optimization

--- config.mk
+++ config.mk
@@ -22,6 +22,8 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
        `$(PKG_CONFIG) --libs freetype2` \
        `$(PKG_CONFIG) --libs harfbuzz`
 
+CFLAGS= -O3 -march=native -pipe
+
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
 STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
